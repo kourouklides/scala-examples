f8ed7c7ce0289327c7363883947dd289ae768281
Ioannis Kourouklides <leonariso@gmail.com>
2019-09-28 21:42:31 +0300

    Create ImmutableVsMutable.scala

---
+++ Examples/Basics/ImmutableVsMutable.scala
@@ -0,0 +1,14 @@
+object ImmutableVsMutables
+{
+    def main(args: Array[String])
+    {
+        // val is immutable
+        val x = 7 + 3
+		val y = x + 1
+		val z = x / 2
+
+        // var is mutable
+        var modifiable = "original value"
+        modifiable = "new value"
+    }
+}
