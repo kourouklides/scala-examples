763871926a9c8d0cbbefeb4b14c567615e6ed6c2
Ioannis Kourouklides <leonariso@gmail.com>
2019-09-30 09:18:34 +0300

    Create Tuples.scala

---
+++ Examples/Learning/Tuples.scala
@@ -0,0 +1,13 @@
+object Tuples
+{
+    def main(args: Array[String])
+    {
+        def average(a: Double, b: Double): Double =
+        {
+            (a + b) / 2.0
+        }
+
+        println(average(10.0, 20.0))
+
+    }
+}
