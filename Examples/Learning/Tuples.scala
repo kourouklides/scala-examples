fa6775b79af0a01cccbe076ceee8562bc4884800
Ioannis Kourouklides <leonariso@gmail.com>
2019-10-05 15:29:45 +0300

    Update Tuples.scala

--- Examples/Learning/Tuples.scala
+++ Examples/Learning/Tuples.scala
@@ -8,10 +8,12 @@ object Tuples
         }
 
         def swap[A,B](p: (A,B)): (B,A) = (p._2, p._1)
+        def swap2[A, B](p: (A, B)): (B, A) = p.swap
+        def swap3[A,B](a:A, b:B): (B, A) = (b,a)
 
         println(average(10.0, 20.0))
         println(swap("Web", 2.0))
         println(swap (20, 30))
-        
+
     }
 }
